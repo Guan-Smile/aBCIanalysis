305ddd0462fb0384311a98236a5e652589e5beae
Guan-Smile <521094300@qq.com>
2019-03-16 14:19:50 +0800

    new

--- guan_deal.m
+++ guan_deal.m
@@ -39,17 +39,17 @@ ALL_fortrain=ALL_fortrain(randIndex,:);
 
 %% my deal _ for other run
  X_test = ALL_fortest(:,1:150);%X;
-yfit(1,:) = trainedModel.predictFcn(X_test);%
-yfit(2,:) = trainedModel1.predictFcn(X_test);%svmc
-yfit(3,:)= trainedModel2.predictFcn(X_test);%svmq
-yfit(4,:)= trainedModel3.predictFcn(X_test); %es SD
+yfit(1,:) = trainedModel121.predictFcn(X_test);%
+% yfit(2,:) = trainedModel1.predictFcn(X_test);%svmc
+% yfit(3,:)= trainedModel2.predictFcn(X_test);%svmq
+% yfit(4,:)= trainedModel3.predictFcn(X_test); %es SD
 
 Y_targetTrue = ALL_fortest(:,151);%Y %%测试集真实标签
  
 accrat =length(find(squeeze(yfit(1,:)) == Y_targetTrue'))/length(Y_targetTrue)
-accrat1 =length(find(squeeze(yfit(2,:)) == Y_targetTrue'))/length(Y_targetTrue)
-accrat2 =length(find(squeeze(yfit(3,:)) == Y_targetTrue'))/length(Y_targetTrue)
-accrat3 =length(find(squeeze(yfit(4,:)) == Y_targetTrue'))/length(Y_targetTrue)
+% accrat1 =length(find(squeeze(yfit(2,:)) == Y_targetTrue'))/length(Y_targetTrue)
+% accrat2 =length(find(squeeze(yfit(3,:)) == Y_targetTrue'))/length(Y_targetTrue)
+% accrat3 =length(find(squeeze(yfit(4,:)) == Y_targetTrue'))/length(Y_targetTrue)
 
 %% plot
 
@@ -67,7 +67,7 @@ title('预测结果与真实情况的对比')
 % accrate=1-num_err/length(Y)
 %% plot2 -accrate
 ap=zeros(1,length(Y_targetTrue));
-[ap_yy,ap_xx]=find(squeeze(yfit(3,:)) == Y_targetTrue');
+[ap_yy,ap_xx]=find(squeeze(yfit(1,:)) == Y_targetTrue');
 ap(ap_xx)=1;
 plot(ap,'-p')
 
